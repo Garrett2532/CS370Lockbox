f115dd33ef0cd502a6f68178a1275bb257c794d7
nd0905 <nd0905@gmail.com>
2021-04-26 12:33:26 -0600

    Added comments cleaned and fixed open close

--- Website.py
+++ Website.py
@@ -13,10 +13,10 @@ password = 'turn' # password for opening the safe
 
 
 class MyServer(BaseHTTPRequestHandler):
-    
+
     def do_GET(self):
         self.standard_out() # Handle a get request with the basic output page
-    
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # get the content from a post request
         post_data = self.rfile.read(content_length) # read the bytes that came in from the post
@@ -30,21 +30,21 @@ class MyServer(BaseHTTPRequestHandler):
         else:
             self.failed_pwd() # similar to standard out but change index to a failed password page
 
-   def failed_pwd(self):
+    def failed_pwd(self):
         self.send_response(401) # unauthorized and bad password
         self.send_header("Content-type", "text/html") # Still send a HTML page for a reattempt
         self.end_headers() # Done with headers
         # Open the file
         with open('www/failed.html', 'rb') as file:
             self.wfile.write(file.read()) # Read the file and send the content
-   def success_pwd(self):
+    def success_pwd(self):
         self.send_response(200) # success safe opening
         self.send_header("Content-type", "text/html") # send a html file to show safe opening
         self.end_headers() # Done with headers
         # Open the file
         with open('www/success.html', 'rb') as file:
             self.wfile.write(file.read()) # Read the file and send the content
-   def standard_out(self):
+    def standard_out(self):
         self.send_response(200) # sucess got to the website
         self.send_header("Content-type", "text/html") # send HTML page
         self.end_headers() # dont with header
@@ -53,21 +53,16 @@ class MyServer(BaseHTTPRequestHandler):
             self.wfile.write(file.read()) # Read the file and send the content
 
 def run():
-    #try:
-    #    while(True):                                      #scans for the propper ID
-    #       id, text = reader.read()
-    #       time.sleep(.2)
-    #       print(id)
-    #       if(id == 786697983187):
-    #             myServer.serve_forever()                 #open server if propper ID given
-    #except KeyboardInterrupt:
-    #    pass
-    try:
-        myServer.serve_forever()
-    except KeyboardInterrupt:
+    try:                                                # Try to run this forever unless something stops it 
+        while(True):                                      #scans for the propper ID
+           id, text = reader.read()                     # read the reader to get the id from the RFID tag
+           time.sleep(.2)                               # wait for it to clear the buffer
+           if(id == 786697983187):                      # if ID is correct 
+                 myServer.serve_forever()                 # open server
+    except KeyboardInterrupt:                           # if the keyboard sends interupt then end it
         pass
-    myServer.server_close()
-    GPIO.cleanup()
+    myServer.server_close()                             # stop the server if its running
+    GPIO.cleanup()                                      # clean up all GPIO
 
-if __name__ == '__main__':
-    run()
+if __name__ == '__main__':                              # Run when called from CLI
+    run()                                               # go into run fuction
--- scripts/ServoRunner.py
+++ scripts/ServoRunner.py
@@ -1,14 +1,38 @@
-import RPi.GPIO as GPIO             
-import time
-servoPIN = 11
-buttonPIN = 40
+import RPi.GPIO as GPIO  # Import GPIO to use button and Servo            
+import time # time needed to auto close the safe after 5 seconds no button
+servoPIN = 11 # Pin the servo is on
+buttonPIN = 40 # Pin the Butto is on
+wait_time = 5 # Time to wait untill auto close the safe
+closed = 1 # widths of pluses that mean closed to the servo
+opened = 5 # widths of pluses that mean open to the servo
 
+
+GPIO.setmode(GPIO.BOARD)            #other GPIO mode was giving errors
+GPIO.setup(servoPIN, GPIO.OUT)      # This sets up the Servo to get data out
+GPIO.setup(buttonPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # This sets up the button to send data in
+p = GPIO.PWM(servoPIN, 20)          # sets up the servo GPIO pin 11 for PWM with 20Hz
+p.start(closed)                     # Starts the servo pwm with a width of closed
+
+def open():
+    p.ChangeDutyCycle(opened)       # Sets the width to opened on the servo
+
+def close():
+    start_time = time.time()        # getting the intial time of calling closed which should be right after open
+    while(True):                    # loop untill condition breaks from the waiting
+        input_state = GPIO.input(buttonPIN) #scans for the push of a button that is connected to the pi
+        if(input_state == False):           #button pressed
+            p.ChangeDutyCycle(closed)       # closes the servo by setting the width to closed
+            time.sleep(.7)                  # sleep for enough time to move the servo 
+            p.stop()                        # stop the servo controler
+            break                           # Finished with the loop
+        if(time.time()-star_time > wait_time):  # check if time elapsed is greater than the time set to wait
+            p.ChangeDutyCycle(closed)       # closes the servo
+            time.sleep(.7)                  # sleeps for the time needed to move the servo
+            p.stop()                        # stops the servo controler
+            break                           # Finished the loop
+
+# Code to test opening and closing with button
 def test():
-    GPIO.setmode(GPIO.BOARD)            #other GPIO mode was giving errors
-    GPIO.setup(servoPIN, GPIO.OUT)
-    GPIO.setup(buttonPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    p = GPIO.PWM(servoPIN, 20) # GPIO 17 for PWM with 20Hz
-    p.start(1)
 
     time.sleep(.5)                      #these values will need to be chnaged 
     p.ChangeDutyCycle(5)
@@ -28,6 +52,7 @@ def test():
             time.sleep(0.2)
             p.stop()
             break
-           # website.closeServer()
+          
+
 if __name__ == '__main__':
     test()
